a878f1f8e84f0fe45745865ea727598127e2dfd3
Victor Mora <victor@turo.com>
2026-02-03 16:44:26 +0000

    ci: use open-turo eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import openTuroTypescriptConfig from "@open-turo/eslint-config-typescript"
+
+// eslint-disable-next-line import/no-default-export
+export default openTuroTypescriptConfig({
+    ignores: ["dist", "lib", "reports", "jest.config.ts"],
+    testFramework: "vitest",
+    typescript: false,
+})
